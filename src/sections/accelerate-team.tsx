92f853d3efe878ef67de1631f1e4970ca9957ea4
zerihun.tegenu <zerihun.tegenu@eaglelionsystems.com>
2025-07-16 23:58:21 +0300

    done until accelerate your team

--- src/sections/accelerate-team.tsx
+++ src/sections/accelerate-team.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import {
+  CalendarArrowUp,
+  Clock,
+  Thermometer,
+  ThermometerSun,
+} from "lucide-react";
 import React, { useState } from "react";
 
 const AccelerateTeam = () => {
@@ -39,6 +45,105 @@ const AccelerateTeam = () => {
           />
         </div>
         <h4 className="text-xl md:text-3xl font-bold text-[#3B0764]">Auxi</h4>
+      </div>
+
+      <div className="max-w-3xl flex items-center gap-6 py-12">
+        <div className="bg-[#1E293B] text-white rounded-lg p-6 w-full max-w-sm shadow-md mx-auto text-center py-12">
+          <h2 className="text-lg font-semibold mb-2">Build it Yourself</h2>
+          <p className="text-sm text-gray-400 mb-4">
+            Long dev cycles, constant maintenance, and a team distracted from
+            core priorities.
+          </p>
+
+          <div className="flex justify-center my-4">
+            {/* Decorative SVG */}
+            <svg width="60" height="60" viewBox="0 0 100 100">
+              <circle
+                cx="50"
+                cy="50"
+                r="40"
+                stroke="#4FB1FF"
+                strokeWidth="4"
+                fill="none"
+              />
+              <path
+                d="M50 10 A40 40 0 1 1 49.9 10"
+                stroke="#4FB1FF"
+                strokeWidth="2"
+                fill="none"
+              />
+            </svg>
+          </div>
+
+          <div className="border-t border-gray-700 my-4"></div>
+
+          <div className="flex justify-between text-sm text-gray-300">
+            <div className="flex flex-col gap-2">
+              <span>Time to launch</span>
+
+              <div className="flex items-center justify-between">
+                <span className="font-semibold text-white">Months</span>
+                <CalendarArrowUp size={22} color="#FCA5A5" strokeWidth={1} />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span>Ongoing maintenance</span>
+
+              <div className="flex items-center justify-between">
+                <span className="font-semibold text-white">High</span>
+                <ThermometerSun size={22} color="#FCA5A5" strokeWidth={1} />
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="bg-black text-white rounded-lg p-6 w-full max-w-sm shadow-md mx-auto text-center py-12">
+          <h2 className="text-lg font-semibold mb-2">With Auxi</h2>
+          <p className="text-sm text-gray-400 mb-4">
+            Launch in days, automate instantly, and keep your team focused on
+            what matters.
+          </p>
+
+          <div className="flex justify-center my-4">
+            <svg width="60" height="60" viewBox="0 0 100 100">
+              <circle
+                cx="50"
+                cy="50"
+                r="40"
+                stroke="#4FB1FF"
+                strokeWidth="4"
+                fill="none"
+              />
+              <path
+                d="M50 10 A40 40 0 1 1 49.9 10"
+                stroke="#4FB1FF"
+                strokeWidth="2"
+                fill="none"
+              />
+            </svg>
+          </div>
+
+          <div className="border-t border-gray-700 my-4"></div>
+
+          <div className="flex justify-between text-sm text-gray-300">
+            <div className="flex flex-col gap-2">
+              <span>Time to launch</span>
+
+              <div className="flex items-center justify-between">
+                <span className="font-semibold text-white">Days</span>
+                <Clock size={22} color="#86EFAC" strokeWidth={1} />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2">
+              <span>Ongoing maintenance</span>
+
+              <div className="flex items-center justify-between">
+                <span className="font-semibold text-white">Zero</span>
+                <Thermometer size={22} color="#86EFAC" strokeWidth={1} />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
